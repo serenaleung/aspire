c997e51338748e6214d9a63d09812bfa1a2113e1
Serena Leung <serenaleung1@gmail.com>
2022-03-25 01:36:23 -0700

    Add filter, add and delete function

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 export default function Results() {
     const [data, setData] = useState([]);
     const [value, setValue] = useState('');
+    const [filteredData, setFilteredData] = useState([]);
+    const [enteredEmails, setEnteredEmails] = useState([]);
 
     const getData=() => {
         fetch('data.json')
@@ -21,13 +23,52 @@ export default function Results() {
     },[])
 
     const handleChange = e => {
-        setValue(e.target.value);
+        setValue(e.target.value.toLowerCase());
+        if (e.target.value !== '' ) {
+            let filteredData = [...data].filter((email) => {
+                return email.toLowerCase().includes(e.target.value)
+                // return (email.toLowerCase().startsWith(e.target.value)) 
+            }) 
+        setFilteredData(filteredData);
+        } 
+    }
+
+    const addEmail = e => {
+        if (e.currentTarget.textContent){
+            let enteredEmail = e.currentTarget.textContent.trim();
+            if(enteredEmail) {
+                setEnteredEmails([...enteredEmails, enteredEmail]);
+                setValue(e.currentTarget.textContent);
+            }
+        }   
+        else if (['Enter', ','].includes(e.key)) {
+            e.preventDefault();
+        let enteredEmail = value.trim();
+            if(enteredEmail) {
+                setEnteredEmails([...enteredEmails, enteredEmail]);
+            }   
+        }
+    }
+
+    const handleDelete = removeEmail => {
+        let updateEmails = [...enteredEmails];
+        setEnteredEmails(updateEmails.filter(email => email !== removeEmail));
     }
 
     return (
         <div>
-            <input placeholder='Enter recipients...' onChange={handleChange} value={value} type="text"></input> 
-            <div>{data}</div>
+            <input placeholder='Enter recipients...' onChange={handleChange} onKeyDown={addEmail} value={value} type="text"></input>
+            {enteredEmails.map((enteredEmail) => 
+                    <div key={enteredEmail}>{enteredEmail}
+                    <button type="button" onClick={()=>handleDelete(enteredEmail)}>x</button>
+                    </div>
+            )}  
+                <div>---------</div>
+
+       
+            {filteredData.map((email, index) => (
+                    <div onClick={addEmail} key={index}>{email}</div>
+                ))}
         </div>
     )
         
